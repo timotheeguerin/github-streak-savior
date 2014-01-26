e314dfc69a292f79c87800ae9cf253fe0b15c998
Timothee Guerin <timothee.guerin@outlook.com>
2014-01-26 16:55:06 -0500

    check commit

--- github_streak_savior.rb
+++ github_streak_savior.rb
@@ -1,15 +1,25 @@
-require_relative 'config.yml'
+require 'yaml'
+require 'open-uri'
 
 class GithubStreakSavior
 
 
-  def config
-    if @c
+  def self.config
+    @config ||= YAML::load(File.open('config.yml'))
+    @config
+  end
+
+
+  def self.get_commit_today
+    url = "https://github.com/users/#{config['username']}/contributions_calendar_data"
+    puts url
+    response = URI.parse(url).read
+    contributions = eval(response)
+    contributions.last[1]
   end
 
   def self.has_commited_today?
-    url = "https://github.com/users/#{GSSConfig.USERNAME}/contributions_calendar_data"
-    Net::HTTP.get(url)
+    get_commit_today > 0
   end
 
 end
