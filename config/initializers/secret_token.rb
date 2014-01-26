8e6b811a51f5022776760d9dd18deda56b9160bf
Timothee Guerin <timothee.guerin@outlook.com>
2014-01-26 15:03:25 -0500

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GithubStreakSavior::Application.config.secret_key_base = '8f4a93b6854a560d6661f4c1b2ca803760dfdb95bdba81c9e4ebff31547099bd3f380c9f93871a400c400e6b40d46a8b115dcb10cd9dcbee72b104e045f5da27'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GithubStreakSavior::Application.config.session_store :cookie_store, key: '_github-streak-savior_session'
